9a8514fa5c74824e8cbc324e573af936876524ba
Wille Marcel <wille.yyz@gmail.com>
2024-05-07 10:13:45 -0300

    Validate yaml files using superstruct lib

---
+++ task/tests/validate-yaml.test.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const {
+  array,
+  assert,
+  boolean,
+  number,
+  object,
+  optional,
+  string,
+} = require('superstruct');
+const { readCampaignYaml } = require('../src/utils');
+
+const structure = array(
+  object({
+    name: string(),
+    platforms: array(
+      object({
+        name: string(),
+        tsv_format: optional(boolean()),
+        rename_as_ict: optional(boolean()),
+        process_as_ict: optional(boolean()),
+        coords_divisor: optional(number()),
+        files: array(string()),
+      })
+    ),
+  })
+);
+
+test('YAML files should be valid', () => {
+  const campaigns = fs.readdirSync('../campaigns/');
+  campaigns
+    .filter((c) => fs.existsSync(path.join('../campaigns/', c, 'deployments.yaml')))
+    .forEach((c) => {
+      console.log(`Checking: ${c}:`);
+      assert(readCampaignYaml(path.join('../campaigns/', c)), structure);
+    });
+});
