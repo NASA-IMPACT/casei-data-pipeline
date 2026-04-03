b1b6c77f651270e37eec6a906a7fc4415d634a12
Wille Marcel <wille.yyz@gmail.com>
2026-04-03 16:32:10 -0300

    Add GALE, PREDICT, PECAN and IceSat campaigns

--- task/src/utils.js
+++ task/src/utils.js
@@ -1,7 +1,7 @@
-import fs from 'fs';
-import path from 'path';
-import { parse } from 'yaml';
-import tar from 'tar';
+import fs from "fs";
+import path from "path";
+import { parse } from "yaml";
+import tar from "tar";
 
 const getMax = (arr) => {
   let len = arr.length;
@@ -29,16 +29,17 @@ const getStats = (arr) => ({
   avg: arr.reduce((a, b) => a + b, 0) / arr.length,
 });
 
-const tsv2csv = (tsvContent) => tsvContent
-  // replace "G 123:" with "123:". 123 can be any number between 1 and 3 digits
-  .replace(/G\s(\d{1,3}):/g, '$1:')
-  // replace " N 1" or " E 1" with " 1"
-  .replaceAll(/\s([NE])\s?(\d{1,3})./g, ' $2.')
-  // replace " W 1" or " S 1" with " -1"
-  .replaceAll(/\s([WS])\s?(\d{1,3})./g, ' -$2.')
-  // replace whitespaces with commas
-  .replace(/( |\t)+/g, ',')
-  .replace(/^,+/gm, '');
+const tsv2csv = (tsvContent) =>
+  tsvContent
+    // replace "G 123:" with "123:". 123 can be any number between 1 and 3 digits
+    .replace(/G\s(\d{1,3}):/g, "$1:")
+    // replace " N 1" or " E 1" with " 1"
+    .replaceAll(/\s([NE])\s?(\d{1,3})./g, " $2.")
+    // replace " W 1" or " S 1" with " -1"
+    .replaceAll(/\s([WS])\s?(\d{1,3})./g, " -$2.")
+    // replace whitespaces with commas
+    .replace(/( |\t)+/g, ",")
+    .replace(/^,+/gm, "");
 
 const concatenateFiles = (file1, file2, output) => {
   const data1 = fs.readFileSync(file1);
@@ -53,54 +54,64 @@ const getDeployments = (file) => {
 };
 
 const readCampaignYaml = (campaignPath) => {
-  const campaignFilePath = path.join(campaignPath, 'deployments.yaml');
+  const campaignFilePath = path.join(campaignPath, "deployments.yaml");
   return getDeployments(campaignFilePath);
 };
 
 const getPlatformConfig = (platformPath) => {
-  const campaignConfig = readCampaignYaml(path.resolve(platformPath, '../..'));
-  const deployment = path.basename(path.resolve(platformPath, '../'));
+  const campaignConfig = readCampaignYaml(path.resolve(platformPath, "../.."));
+  const deployment = path.basename(path.resolve(platformPath, "../"));
   const platform = path.basename(platformPath);
-  return campaignConfig
-    .find((d) => d.name === deployment).platforms
-    // As we remove the slashes from the platform folder,
-    // we need to put it back when reading the yaml file
-    .find((p) => p.name === platform || p.name === platform.replaceAll('-', '/'));
+  return (
+    campaignConfig
+      .find(
+        (d) => d.name.replace(/[-/]/g, "") === deployment.replace(/[-/]/g, ""),
+      )
+      .platforms// As we remove the slashes from the platform folder,
+      // we need to put it back when reading the yaml file
+      .find(
+        (p) => p.name === platform || p.name === platform.replaceAll("-", "/"),
+      )
+  );
 };
 
-const divideCoordinates = (features, coordsDivisor) => features.map((i) => {
-  const newGeometry = {
-    ...i.geometry,
-    coordinates: i.geometry.coordinates.map((c) => c / coordsDivisor),
-  };
-  return { ...i, geometry: newGeometry };
-});
+const divideCoordinates = (features, coordsDivisor) =>
+  features.map((i) => {
+    const newGeometry = {
+      ...i.geometry,
+      coordinates: i.geometry.coordinates.map((c) => c / coordsDivisor),
+    };
+    return { ...i, geometry: newGeometry };
+  });
 
 const urlHasFileExtension = (url) => {
-  const ext = path.basename(url).split('.').slice(-1)[0];
+  const ext = path.basename(url).split(".").slice(-1)[0];
   // nc, sb, and h5 are the only extension that don't have a length of 3 or 4 chars
-  if (['nc', 'sb', 'h5'].includes(ext)) return ext;
+  if (["nc", "sb", "h5"].includes(ext)) return ext;
   return ext && [3, 4].includes(ext.length);
 };
 
 const extractFromTar = async (tarFilePath, destination) => {
-  const extensions = ['.sb', '.nc', '.txt'];
-  await tar.list({
-    file: tarFilePath,
-    onentry: (entry) => {
-      if (extensions.includes(path.extname(entry.path))) {
-        entry.pipe(fs.createWriteStream(`${destination}/${entry.path}`));
+  const extensions = [".sb", ".nc", ".txt"];
+  await tar.list(
+    {
+      file: tarFilePath,
+      onentry: (entry) => {
+        if (extensions.includes(path.extname(entry.path))) {
+          entry.pipe(fs.createWriteStream(`${destination}/${entry.path}`));
+        } else {
+          entry.resume(); // Skip non-.txt files
+        }
+      },
+    },
+    (err) => {
+      if (err) {
+        console.error("Error extracting tar file:", err);
       } else {
-        entry.resume(); // Skip non-.txt files
+        console.log("Tar file extracted successfully!");
       }
     },
-  }, (err) => {
-    if (err) {
-      console.error('Error extracting tar file:', err);
-    } else {
-      console.log('Tar file extracted successfully!');
-    }
-  });
+  );
 };
 
 export {
